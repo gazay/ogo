80360dab5572f7b6363f7fd6a7882a1cbe765118
Alexey Gaziev <alex.gaziev@gmail.com>
2016-04-26 08:31:23 -0300

    ImageInfo class

--- lib/ogo/image_info.rb
+++ lib/ogo/image_info.rb
@@ -11,6 +11,48 @@ module Ogo
     end
 
     def fetch_size
+      return if @width && @height
+      if defined?(FastImage)
+        @width, @height = fi_check(:size, url)
+      else
+        []
+      end
+    end
+
+    def fetch_size!
+      @width, @height = nil
+      fetch_size
+    end
+
+    def fetch_type
+      @type ||= \
+        if defined?(FastImage)
+          fi_check(:type, url).to_s
+        else
+          uri = Addressable::URI.parse(url).normalize
+          uri.path.split('.').last.to_s
+        end
+    end
+
+    def fetch_type!
+      @type = nil
+      fetch_type
+    end
+
+    def content_type
+      "image/#{fetch_type}"
+    end
+
+    private
+
+    def fi_check(method, url, options=nil)
+      options ||= {raise_on_failure: true, timeout: 2.0}
+      FastImage.send(method, url, options)
+    rescue
+      url = Addressable::URI.parse(url).normalize
+      val = FastImage.send(method, url, options)
+      @url = url
+      val
     end
 
   end
